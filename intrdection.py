281c8f9be6fe06a05b8810140687c364b5d5ce2b
paras01-patel <paraschandrawanshi649@gmail.com>
2026-07-12 11:59:42 +0530

    Python/Introdection

---
+++ intrdection.py
@@ -0,0 +1,74 @@
+'''
+1.what is python 
+ans.Python is a high-level, interpreted, object-oriented programming language. It is easy to learn because of its simple syntax and is widely used for web development, data science, artificial intelligence, automation, and software development.
+
+2.features of python ?
+ans.1.Easy to Learn and Read
+        a.Python ka syntax simple aur English jaisa hota hai.
+        b.Isliye beginners ise jaldi seekh lete hain.
+        
+    2.Interpreted Language
+        a.Python code line by line execute hota hai.
+        b.Compile karne ki zarurat nahi hoti.
+        
+    3.Object-Oriented Programming (OOP)
+        a.Python OOP ko support karta hai.
+        b.OOP me code ko Objects aur Classes ke through organize kiya jata hai.
+        
+    4.Dynamically Typed
+        a.Variable ka data type pehle se declare nahi karna padta.
+        
+    5.High-Level Language
+        a.Python hardware se directly deal nahi karta.
+        b.Memory management aur low-level details Python khud handle karta hai.
+        
+    6.Cross-Platform (Portable)
+        a.Ek hi Python program Windows, Linux aur macOS par chal sakta hai.
+        b.Har operating system ke liye alag code likhne ki zarurat nahi hoti.
+        
+    7.Open Source
+        a.Python free hai.
+        b.Iska source code sabke liye available hai.
+        
+    8.Large Standard Library
+        a.Python me pehle se bahut saare built-in modules hote hain.
+        b.Jaise:math ,randomos,datetime,json
+        
+    9.Automatic Memory Management
+        a.Python automatically unused memory ko free kar deta hai (Garbage Collection).
+
+
+
+
+3.advantages of python ?
+ans. Advantages of Python (Simple)
+    1.Easy to Learn
+        Python ka syntax simple hai, isliye beginners ise aasani se seekh sakte hain.
+    
+    2.Easy to Read and Write
+        Code clean aur English jaisa hota hai, isliye samajhna aur maintain karna easy hota hai.
+    
+    3.Free and Open Source
+        Python bilkul free hai aur koi bhi ise use ya modify kar sakta hai.
+    
+    4.Cross-Platform
+        Ek hi code Windows, Linux aur macOS par chal sakta hai.
+    
+    5.Large Standard Library
+        Python me bahut saare built-in modules aur libraries available hote hain.
+    
+    6.Object-Oriented
+        Classes aur Objects ko support karta hai, jisse code reusable aur organized hota hai.
+    
+    7.Interpreted Language
+        Code line by line execute hota hai, isliye testing aur debugging fast hoti hai.
+    
+    8.Huge Community Support
+        Python ki bahut badi community hai, isliye problems ke solutions aasani se mil jaate hain.
+    
+    9.Versatile
+        Python ka use Web Development, AI, Machine Learning, Data Science, Automation, Game Development, aur Cybersecurity me hota hai.
+    
+    10.High Productivity
+        Kam code me zyada kaam ho jata hai, jisse development fast hota hai.
+'''
